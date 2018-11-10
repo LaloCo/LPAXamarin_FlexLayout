32bde03d9426193a2d86fc128f43efac39dfd3f2
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2018-11-10 06:21:45 -0600

    2121

---
+++ FlexLayoutExample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FlexLayoutExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+    }
+}
